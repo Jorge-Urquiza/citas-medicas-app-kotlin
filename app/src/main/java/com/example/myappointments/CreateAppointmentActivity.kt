6aada4695f5440e7a42f622a33cf32b82177e8b3
Jorge Luis <jorgestudio2017@gmail.com>
2020-04-09 00:36:41 -0400

    Poblar spinner completed

--- app/src/main/java/com/example/myappointments/CreateAppointmentActivity.kt
+++ app/src/main/java/com/example/myappointments/CreateAppointmentActivity.kt
@@ -2,11 +2,30 @@ package com.example.myappointments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_appointment.*
 
 class CreateAppointmentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_appointment)
+        btnNext.setOnClickListener{
+            cvStep1.visibility= View.GONE
+            cvStep2.visibility=View.VISIBLE
+        }
+        btnConfirmAppointment.setOnClickListener{
+            Toast.makeText(this,"Cita registrada correctamente",Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+        val optionSpecialties= arrayOf("Specialty A ", "Specialty B", "Specialty C")
+        spinnerSpecialties.adapter= ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,
+            optionSpecialties)
+        val optionDoctors= arrayOf("Doctor A ", "Doctor B", "Doctor C")
+        spinnerDoctors.adapter= ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,
+            optionDoctors)
     }
 }
